f28a5deb43bd724fcd771cba6d93e215a6f2cb66
liuzongxin <liuzongxin@3tlife.cn>
2023-09-17 19:22:03 +0800

    Spring Bean工具类

---
+++ README.md
@@ -0,0 +1,3 @@
+# SpringBoot General Functions
+
+记录一下工作中遇到的一些与业务无关的通用功能。
---
+++ src/main/java/com/lzx/springbootgeneralfunctions/SpringBootGeneralFunctionsApplication.java
@@ -0,0 +1,13 @@
+package com.lzx.springbootgeneralfunctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGeneralFunctionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGeneralFunctionsApplication.class, args);
+    }
+
+}
